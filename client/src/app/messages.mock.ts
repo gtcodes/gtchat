61a38f14f6012701e5ff12ba91bf2eac3ee99b15
Criticalstone <kevinhoog@gmail.com>
2018-04-13 11:06:08 +0200

    Removes mock data

--- client/src/app/messages.mock.ts
+++
@@ -1,7 +0,0 @@
-import { Message } from './message';
-
-export const MESSAGES: Message[] = [
-    {author: 'Perry', message: 'Hello World!'},
-    {author: 'Kritt', message: 'I know right!'},
-    {author: 'Basse', message: 'You guys are so lame...'}
-];
